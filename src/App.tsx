c93bf0c820ae26e1f43276cdb369e257e0dda135
phelipe <phelegal88@gmail.com>
2025-11-02 18:19:41 -0300

    component and add Card component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-export function App() {
-
+interface ICardProps {
+  title: string
+  children: React.ReactNode
+}
+const Card = (props: ICardProps) => {
   return (
-    <>
+    <div className="card">
+      <span>Title: {props.title}</span>
+
+      <div>
+        {props.children}
+      </div>
+
+      <div>
+        Footer
+      </div>
+
+    </div>
+  )
+}
+
+export function App() {
+  return (
+    <div>
       <h1>Olá, mundo!</h1>
-    </>  
+
+      <p>Card:</p>
+
+      <Card title='teste'>
+        Abacate
+      </Card>
+    </div>  
   )
 }
